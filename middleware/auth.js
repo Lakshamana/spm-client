e081d8dd215af61c24d66f6e79d7e165c309be6f
Lakshamana <guitrompa1@gmail.com>
2019-11-24 14:57:08 -0300

    fix login routing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 export default function({ store, redirect, route }) {
   const auth = store.state.auth
   if (!auth.user || !auth.token) {
-    redirect(`/login?to=${btoa(route.path)}`)
+    redirect(`/login?to=${btoa(route.fullPath)}`)
   }
 }
